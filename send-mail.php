d2a1602f68ebade4885ea6fc82951f028e99fb31
Alejandro Ramirez <48692145+alejandroramirezj@users.noreply.github.com>
2022-06-01 16:29:55 +0200

    Update send-mail.php

--- send-mail.php
+++ send-mail.php
@@ -2,7 +2,7 @@
 
 	// site owner
 	$site_name = 'Correo de la página WEB';
-	$sender_domain = 'postmaster@alejandroramirezjimenez.com';
+	$sender_domain = 'smtp.gmail.com';
 	$to = 'info@alejandroramirezjimenez.com';
 	
 	// contact form fields
